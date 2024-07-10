a4a5a3f0cf4df21d996949003836a76c60972fdf
tncts <shashwat.satvik@gmail.com>
2024-07-10 22:04:55 +0530

    trying something

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+import './App.css'
+
+
+function App() {
+  const [todos , setTodos] = useState([{
+    title: "go to gym",
+    decription: "go to gym from 7-8",
+    completed: false
+  },
+  {
+    title: "study DSA",
+    decription: "study DSA from 9-10",
+    completed: true
+  }])
+
+  return (
+      <div>
+       {JSON.stringify(todos)}
+      </div>
+  )
+}
+
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
+}
+
+export default App
